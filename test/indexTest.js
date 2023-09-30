ea7ab81490bd269a4a047a8927fac4b5a31f9d82
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-09-30 21:44:20 +0300

    Assignment completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,7 @@
 require ( './helpers.js' );
 
-describe('employees', function() {
+describe('employees', function() 
+{
   describe('updateEmployeeWithKeyAndValue(employee, key, value)', function () {
     beforeEach(function () {
       for (const key in employee) {
